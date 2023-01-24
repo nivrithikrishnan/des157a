2ec8bafde5ddc00b931a667f07666c5d9cdf123b
nivrithikrishnan <nivrithi@soumya.com>
2023-01-24 14:19:59 -0800

    uhhh

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-/* Navigation Animations*/
-/* switching tabs */
+var wwidth = window.innerWidth;
 let labels = document.getElementsByClassName("tab-labels");
 let tabs = document.getElementsByClassName("tab-content");
 
-document.getElementById("tab-se").classList.add('tab-active');
-function switchtab(label, name){
+if (wwidth > 1210) {
+    /* Resets CSS Classes for Desktop View */
     for (var i = 0; i < tabs.length; i++){
         tabs[i].style.display = "none";
         labels[i].classList.add('tab-inactive'); 
         labels[i].classList.remove('tab-active'); 
     }
     
-    document.getElementById(label).classList.add('tab-active');
-    document.getElementById(name).style.display = "block"; 
-    console.log("switched to", name);
+    document.getElementById("tab-se").classList.add('tab-active');
+    document.getElementById('container-se').style.display = "block"; 
+
+    /* Navigation Animations*/
+    /* switching tabs */
+    document.getElementById("tab-se").classList.add('tab-active');
+    function switchtab(label, name){
+        for (var i = 0; i < tabs.length; i++){
+            tabs[i].style.display = "none";
+            labels[i].classList.add('tab-inactive'); 
+            labels[i].classList.remove('tab-active'); 
+        }
+        
+        document.getElementById(label).classList.add('tab-active');
+        document.getElementById(name).style.display = "block"; 
+        console.log("switched to", name);
+    }
+} else {
+    /* Resets CSS Classes for Mobile View */
+    document.getElementById(nav).style.display = "flex";
+    for (var i = 0; i < tabs.length; i++){
+        tabs[i].style.display = "block";
+        labels[i].classList.add('tab-active'); 
+        labels[i].classList.remove('tab-inactive'); 
+
+        console.log(tabs[i].style.display, labels[i].classList);
+    }
+
 }
 
 /* Header Animations */
