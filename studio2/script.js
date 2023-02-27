ee0c91da807cda3dcb6fc1d22b4b7dd04b4b208e
nivrithikrishnan <nivrithi@soumya.com>
2023-02-27 15:06:31 -0800

    studio 2 slideshow basics

---
+++ studio2/script.js
@@ -0,0 +1,50 @@
+(function(){
+    'use strict';
+    console.log('reading js'); 
+
+    /* variable declarations */
+    let location = 0;      //slideshow location (previous index)
+    let index = 1;      //index of current image
+    let direction = true;       //direction of carouself movement (true if forwards, false if backwards)
+    let slideshow = document.querySelector("ul");      //slideshow element
+    let current = document.getElementsByTagName("li")[0].firstChild;      //current element of slideshow
+
+    /* starts slideshow on first image */
+    slideshow.style.transform = `translateX(20em)`;
+
+    /* function to start the flideshow */
+    function startSlideshow() {
+        /* variable declarations */
+        let numImages = document.querySelectorAll("img");       //number of images 
+        
+        /* updates direction if necessary */ 
+        if (location === numImages.length - 2) {
+            direction = false; 
+        } else if (location == -1){
+            slideshow.style.transform = `translateX(20em)`;
+            direction = true; 
+        }
+
+        /* moves carousel */
+        slideshow.style.transform = `translateX(-${location * 20}em)`;
+
+        /* transforms current image */
+        console.log(location, current);
+        if (location >= -1){
+            current.classList.remove("active");
+            current = document.getElementsByTagName("li")[index].firstChild; 
+            current.classList.add("active"); 
+        }
+
+        /* increments index based on current direction */
+        (direction) ? location+=1 : location -=1; 
+        (direction) ? index+=1 : index-=1; 
+    }
+
+    /* timer for running the slideshow */
+    setInterval(() => {
+        startSlideshow();
+      }, 2000);
+})
+
+();
